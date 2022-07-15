0852d28c15b7d29c456647ff35a2862fcad070ce
narender <narenymca07@gmail.comm>
2022-07-15 23:08:11 +0530

    day 13 completed

--- sample/lib/pages/home_page.dart
+++ sample/lib/pages/home_page.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: must_be_immutable
 
 import 'package:flutter/material.dart';
+import 'package:sample/models/catalog.dart';
+import 'package:sample/widgets/item_widget.dart';
 
 import '../widgets/drawer.dart';
 
@@ -10,13 +12,21 @@ class HomePage extends StatelessWidget {
   HomePage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         // ignore: prefer_const_constructors
         title: Text("this is day"),
       ),
-      body: Center(
-        child: Text("welcome to $days this app by narender"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+            itemCount: dummyList.length,
+            itemBuilder: (context, index) {
+              return ItemWidget(
+                item: dummyList[index],
+              );
+            }),
       ),
       // ignore: prefer_const_constructors
       drawer: MyDrawer(),
