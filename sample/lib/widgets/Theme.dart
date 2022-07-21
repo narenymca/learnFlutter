a6bac689751a3a15dbdd53ff0d6b6a96dd5236e4
narender <narenymca07@gmail.comm>
2022-07-21 07:05:22 +0530

    day17 completed

--- sample/lib/widgets/Theme.dart
+++ sample/lib/widgets/Theme.dart
@@ -2,9 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class MyTheme {
+// colors
+  static Color creamColor = Color(0xfff5f5f5);
+  static Color darkBluishColor = Color(0xff403b58);
+
   static ThemeData lightTheme(BuildContext context) => ThemeData(
         primarySwatch: Colors.deepPurple,
-        fontFamily: GoogleFonts.lato().fontFamily,
+        fontFamily: GoogleFonts.poppins().fontFamily,
         // ignore: prefer_const_constructors
         appBarTheme: AppBarTheme(
             color: Colors.white,
@@ -21,7 +25,7 @@ class MyTheme {
               color: Colors.black,
             )),
       );
-      static ThemeData darkTheme(BuildContext context) => ThemeData(
+  static ThemeData darkTheme(BuildContext context) => ThemeData(
         primarySwatch: Colors.deepPurple,
         fontFamily: GoogleFonts.lato().fontFamily,
         // ignore: prefer_const_constructors
